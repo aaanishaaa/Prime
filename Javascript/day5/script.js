746933389397fa49354dfbc9b09785bc4be5248b
aaanishaaa <anishaworks21@gmail.com>
2025-02-19 16:11:32 +0530

    day 5

--- Javascript/day5/script.js
+++ Javascript/day5/script.js
@@ -18,12 +18,13 @@
 // document.children[0].children[1].children[1].children[1].style.color='yellow';
 // document.children[0].children[1].children[1].children[2].style.color='yellow';
 // document.children[0].children[1].children[1].children[3].style.color='yellow';
-// const outerdiv=document.querySelector("div");
-// outerdiv.style.backgroundColor='blue';
-// outerdiv.style.padding='20px';
-// for(let i=0;i<outerdiv.children.length;i++){
-//     outerdiv.children[i].style.backgroundColor='yellow';
-// }
+const outerdiv=document.querySelector("div");
+outerdiv.style.backgroundColor='blue';
+outerdiv.style.padding='10px';
+outerdiv.style.color='blue';
+for(let i=0;i<outerdiv.children.length;i++){
+    outerdiv.children[i].style.backgroundColor='yellow';
+}
 
 const mapping={
     Invitation:"You are invited for the event",
@@ -31,3 +32,11 @@ const mapping={
     Notice:"Notice for the event",
     Meeting:"Meeting is scheduled",
 };
+
+Array.from(outerdiv.children).forEach((child) => {
+    if (child.children[0] && child.children[1]) {
+        const key = child.children[0].innerHTML;
+        console.log(key);
+        child.children[1].innerHTML = mapping[key];
+    }
+});
